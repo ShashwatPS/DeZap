4fb56df3000d1c2d2228dbeccb440d02cee1ba2a
Shashwat Singh <shashwatsingh@Shashwats-MacBook-Pro.local>
2025-02-27 23:14:46 +0530

    Slack working

--- dezap_front/src/constants/url.ts
+++ dezap_front/src/constants/url.ts
@@ -1,4 +1,4 @@
 export const URL="http://localhost:3000"
 export const SLACK_CLIENT_ID ="5875039330148.8519347852739"
 export const SLACK_CLIENT_SECRET="236ef261106f48d29c90b0298601d07f"    
-export const SLACK_REDIRECT_URI="https://004d-2409-40d2-2c-d436-59e4-2cbf-658a-4ec.ngrok-free.app/slackRedirect"
+export const SLACK_REDIRECT_URI="https://64c9-2409-40d2-2c-d436-8ceb-83a0-361c-676b.ngrok-free.app/slackRedirect"
